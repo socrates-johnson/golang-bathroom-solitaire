bce127e27acdbc6cfd3fc12653cee1fad3b03772
Tom Pavlak <thomas.a.pavlak@gmail.com>
2017-02-25 09:05:59 -0800

    Add buildDeck function and clean up shuffle

--- bathroom-solitaire.go
+++ bathroom-solitaire.go
@@ -16,46 +16,55 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 //==============================================================================
 // Main program
 //==============================================================================
 func main() {
-	suits := []string{"h", "d", "c", "s"}
-	cards := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"}
-	deck_0 := make([]string, 52)
+	rand.Seed(time.Now().UnixNano())
 
+	// build base deck
+	deck_0 := buildDeck()
+
+	// shuffle deck
+	deck := shuffle(deck_0)
+
+	fmt.Println(deck_0)
+	fmt.Println()
+	fmt.Println(deck)
+}
+
+
+//==============================================================================
+// Deck building function
+//==============================================================================
+func buildDeck() []string {
+	suits := []string{"h", "d", "c", "s"}
+	cards := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J",
+		              "Q", "K"}
+
+	deck := make([]string, 52)
 	counter := 0
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 13; j++ {
-			deck_0[counter] = cards[j] + suits[i]
+			deck[counter] = cards[j] + suits[i]
 			counter += 1
 		}
 	}
-
-	// shuffle deck 7 times
-	deck := shuffle(deck_0, 7)
-
-	fmt.Println(suits)
-	fmt.Println(suits[:2])
-	fmt.Println(cards)
-	fmt.Println(deck_0)
-	fmt.Println(deck)
-	// fmt.Println(shuffle(deck_0, 1))
+	return deck
 }
 
+
 //==============================================================================
-// Function to shuffle deck
+// Shuffle function
 //==============================================================================
-func shuffle(deck_in []string, n int) []string {
+func shuffle(deck_in []string) []string {
 	deck_out := make([]string, len(deck_in))
-	for i := 0; i < n; i++ {
-		perm := rand.Perm(len(deck_in))
-		for i, v := range perm {
-			deck_out[v] = deck_in[i]
-		}
-		deck_in = deck_out
+	perm := rand.Perm(len(deck_in))
+	for i, v := range perm {
+		deck_out[v] = deck_in[i]
 	}
 	return deck_out
 }
